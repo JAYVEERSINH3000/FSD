5f36f987dbb47ba8ad1eb8eb436bd55563aae94e
JAYVEERSINH3000 <parmarjayveer3000@gmail.com>
2026-04-13 13:21:16 +0530

    Delete FSD-2/T2(express js)/RootParameter-1.js

--- FSD-2/T2(express js)/RootParameter-1.js
+++
@@ -1,7 +0,0 @@
-// Root parameters
-var express=require("express");
-var app=express();
-app.get("/exam/:phase/marks/:fsd2",(req,res)=>{
-   res.send(req.params);  // if only want phase than req.params.phase
-})
-app.listen(7008);
