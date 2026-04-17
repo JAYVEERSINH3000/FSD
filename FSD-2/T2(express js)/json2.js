b3507316f96ee2096162c68bd0a7e1bbdcdd0f5b
JAYVEERSINH3000 <parmarjayveer3000@gmail.com>
2026-04-17 12:10:02 +0530

    Delete FSD-2/T2(express js)/json2.js

--- FSD-2/T2(express js)/json2.js
+++
@@ -1,9 +0,0 @@
-// json proccesing
-var express=require("express");
-var app=express();
-student={'name':'jayveer','age':19}
-app.get("/j1",(req,res)=>{
-   res.write(student.age+" ");  // always pass string 
-   res.send()
-})
-app.listen(3333);
