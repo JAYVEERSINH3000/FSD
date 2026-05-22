02ea4c39f0c1fe99d4db705c4534c3f9cb50f43b
JAYVEERSINH3000 <parmarjayveer3000@gmail.com>
2026-05-22 12:31:45 +0530

    Delete FSD-2/T3(react)/Chapter-7/Child.jsx

--- FSD-2/T3(react)/Chapter-7/Child.jsx
+++
@@ -1,8 +0,0 @@
-function Child(props){
-   return(
-      <h1 style={{backgroundColor:'yellow',color:'red'}}>
-         Welcome {props.data.name} to {props.data.clg}
-      </h1>
-   )
-}
-export default Child;
