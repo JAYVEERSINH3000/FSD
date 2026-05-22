4ff40c24e6b4981f74e8f56949329b0e094adc17
JAYVEERSINH3000 <parmarjayveer3000@gmail.com>
2026-05-22 12:31:31 +0530

    Delete FSD-2/T3(react)/Chapter-7/Prop2.jsx

--- FSD-2/T3(react)/Chapter-7/Prop2.jsx
+++
@@ -1,9 +0,0 @@
-function Prop2(props){
-   return(
-      <div>
-         <h1 style={{color:'red',textDecoration:"underline"}}>{props.name}</h1>
-         <h2>{props.age+1}</h2>
-      </div>
-   )
-}
-export default Prop2;
